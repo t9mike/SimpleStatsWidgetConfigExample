65c73b449a1576f72bcda283f17cf8cd30b1262e
Michael S Muegel <mike@muegel.org>
2025-07-13 16:18:41 -0500

    Initial Commit

---
+++ SimpleStats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleStats
+//
+//  Created by Michael S. Muegel on 7/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleStats/SimpleStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStatsApp.swift
+//  SimpleStats
+//
+//  Created by Michael S. Muegel on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
